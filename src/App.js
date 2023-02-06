ff87437e9a7b2cb0a58d7ce1365b004ca314f372
John Nguyen <JohnNguyen11@johns-mbp-6.lan>
2023-02-05 22:43:25 -0500

    add todos client-side

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
+import React, { Fragment } from "react";
 import './App.css';
+
+
+//components
+
+import InputTodo from "./components/InputTodo";
+// import ListTodos from "./components/ListTodos";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        hi
-      </h1>
-    </div>
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        {/* <ListTodos /> */}
+      </div>
+    </Fragment>
   );
 }
-
 export default App;
